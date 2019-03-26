e920e1dd183ecda15f9cc4c65dc1ac98479ea909
MarcoGorelli <m.e.gorelli@gmail.com>
2019-03-26 20:24:22 +0000

    Fix config file for docs so it's PEP8-compliant

--- docs/conf.py
+++ docs/conf.py
@@ -130,7 +130,8 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'Pysprint-Mar-26-2019.tex', 'Pysprint-Mar-26-2019 Documentation',
+    (master_doc, 'Pysprint-Mar-26-2019.tex',
+     'Pysprint-Mar-26-2019 Documentation',
      'DS from scratch', 'manual'),
 ]
 
