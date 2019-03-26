7a04b7c5811d5cbc624ec2be9c4b3101e659a348
Kara de la Marck <karadelamarck@gmail.com>
2019-03-26 20:05:01 +0000

    Start of flask api

---
+++ api.py
@@ -0,0 +1,13 @@
+import flask
+from flask import request, jsonify
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+
+@app.route('/', methods=['GET'])
+def home():
+    return '''<h1>Welcome to Titanic Predictor</h1>
+<p>A prototype API for predicting who survives the Titanic.</p>'''
+
+app.run()
