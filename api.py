f63a061cfbacff9b3ee0d3179edd0858bb011f98
Kara de la Marck <karadelamarck@gmail.com>
2019-03-26 20:27:47 +0000

    Fix formatting errors

--- api.py
+++ api.py
@@ -1,5 +1,5 @@
 import flask
-from flask import request, jsonify
+# from flask import request, jsonify
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
@@ -9,5 +9,6 @@ app.config["DEBUG"] = True
 def home():
     return '''<h1>Welcome to Titanic Predictor</h1>
 <p>A prototype API for predicting who survives the Titanic.</p>'''
+
 
 app.run()
