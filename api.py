1875de53fd616c58d53a3cb7304ae3c0801c4f43
Kara de la Marck <karadelamarck@gmail.com>
2019-03-26 22:23:01 +0000

    Add mock POST request

--- api.py
+++ api.py
@@ -1,14 +1,28 @@
 import flask
-# from flask import request, jsonify
+from flask import request, jsonify
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
+books = [
+    {'id': 1,
+     'title': 'The Ones Who Walk Away From Omelas',
+     'author': 'Ursula K. Le Guin',
+     'first_sentence': 'With a clamor of bells that set the swallows soaring, the Festival of Summer came to the city Omelas, bright-towered by the sea.',
+     'published': '1973'}
+     ]
 
 @app.route('/', methods=['GET'])
 def home():
     return '''<h1>Welcome to Titanic Predictor</h1>
 <p>A prototype API for predicting who survives the Titanic.</p>'''
+
+
+@app.route('/books', methods=['POST'])
+def add_one():
+    new_book = request.get_json()
+    books.append(new_book)
+    return jsonify({'books' : books})
 
 
 app.run()
