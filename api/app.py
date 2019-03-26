4d2c8ead303e42979d81d15aa5afafc60bd91805
Andy <andrew@appsumer.io>
2019-03-26 19:42:35 +0000

    Latest updates

--- api/app.py
+++ api/app.py
@@ -1,9 +1,11 @@
-from flask import request, url_for
-from flask_api import FlaskAPI, status, exceptions
-from model import Model, allowed_values
+
+from flask import request
+from flask_api import FlaskAPI
+# from ../model import Model, allowed_values
 
 
 app = FlaskAPI(__name__)
+
 
 @app.route("/titanic", methods=['GET', 'POST'])
 def titanic():
@@ -12,12 +14,11 @@ def titanic():
     else:
         return "Fuck Off!"
 
-    
-
 
 @app.route("/", methods=['GET'])
 def index():
     return "Hey Guys!"
-    
+
+
 if __name__ == "__main__":
     app.run(debug=True)
